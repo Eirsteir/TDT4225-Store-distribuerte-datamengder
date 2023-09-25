8f3797dd48c8608e100b2d6159c16d73b141d271
Eirik Steira <steiraeirik@gmail.com>
2023-09-25 12:45:39 +0200

    Remove batching of users

--- insert.py
+++ insert.py
@@ -14,7 +14,6 @@ class DataLoader:
     def load_users(self):
         data_dir = "./dataset"
         user_records = []
-        batch_size = 100
 
         with open(data_dir + "/labeled_ids.txt", "r") as labeled_ids:
             user_ids_with_labels = [line.strip() for line in labeled_ids]
@@ -23,14 +22,7 @@ class DataLoader:
             has_labels = user_id in user_ids_with_labels
             user_records.append((user_id, has_labels))
 
-            if len(user_records) >= batch_size:
-                self.cursor.executemany("INSERT INTO User (id, has_labels) VALUES (%s, %s)", user_records)
-                user_records = []  # Clear the batch for the next set of records
-
-        # Insert any remaining records that didn't reach the batch size
-        if user_records:
-            self.cursor.executemany("INSERT INTO User (id, has_labels) VALUES (%s, %s)", user_records)
-
+        self.cursor.executemany("INSERT INTO User (id, has_labels) VALUES (%s, %s)", user_records)
         self.connection.db_connection.commit()
         print(self.cursor.rowcount, "Record inserted successfully into Laptop table")
 
