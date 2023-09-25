37aac557c0fac7b63a55960a52120c45de07b4d6
Jørgen Halse <jrgenhalse@gmail.com>
2023-09-25 12:30:20 +0200

    create main function in insert.py

--- insert.py
+++ insert.py
@@ -12,25 +12,29 @@ class DataLoader:
         self.cursor = self.connection.cursor
 
     def load_users(self):
-        data_dir = "/dataset"
+        data_dir = "./dataset"
         user_records = []
         batch_size = 100
 
         with open(data_dir + "/labeled_ids.txt", "r") as labeled_ids:
             user_ids_with_labels = [line.strip() for line in labeled_ids]
 
-        for user_id in os.listdir(data_dir):
+        for user_id in os.listdir(data_dir + "/Data"):
             has_labels = user_id in user_ids_with_labels
             user_records.append((user_id, has_labels))
 
             if len(user_records) >= batch_size:
-                self.cursor.executemany("INSERT INTO User (user_id, has_labels) VALUES (%s, %s)", user_records)
+                self.cursor.executemany("INSERT INTO User (id, has_labels) VALUES (%s, %s)", user_records)
                 user_records = []  # Clear the batch for the next set of records
 
         # Insert any remaining records that didn't reach the batch size
         if user_records:
-            self.cursor.executemany("INSERT INTO User (user_id, has_labels) VALUES (%s, %s)", user_records)
+            self.cursor.executemany("INSERT INTO User (id, has_labels) VALUES (%s, %s)", user_records)
 
 
+if __name__ == "__main__":
+    loader = DataLoader()
+    loader.load_users()
+    loader.connection.close_connection()
 
 
