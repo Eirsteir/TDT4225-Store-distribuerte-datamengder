b4982558caaa02a260b3ea34fce9ceb6ba7ac8b2
Anna <annabjorgo0308@gmail.com>
2023-09-30 23:02:59 +0200

    proposal for question 1-4

--- queries.py
+++ queries.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 from DbConnector import DbConnector
 
+
 class Queries:
 
     def __init__(self):
@@ -8,23 +9,63 @@ class Queries:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    # test query from example.py
-    def fetch_data(self, table_name):
-        query = "SELECT * FROM %s"
+    def query_one(self, table_name):
+        query = "SELECT COUNT(*) FROM %s"
         self.cursor.execute(query % table_name)
-        rows = self.cursor.fetchall()
-        print("Data from table %s, raw format:" % table_name)
-        print(rows)
-        # Using tabulate to show the table in a nice way
-        print("Data from table %s, tabulated:" % table_name)
-        print(tabulate(rows, headers=self.cursor.column_names))
+        result = self.cursor.fetchone()
+        print(f"Entries in {table_name}: {result[0]}")
+
+    def query_two(self):
+        number_of_trackpoints_per_user = "SELECT User.id, " \
+                                         "  COUNT(TrackPoint.id) AS Number_of_trackpoints " \
+                                         "FROM User " \
+                                         "INNER JOIN Activity ON User.id = Activity.user_id " \
+                                         "INNER JOIN TrackPoint ON Activity.id = TrackPoint.activity_id " \
+                                         "GROUP BY User.id"
+
+        query = "SELECT AVG(Number_of_trackpoints), " \
+                "       MAX(Number_of_trackpoints), " \
+                "       MIN(Number_of_trackpoints) " \
+                "FROM ( %s ) AS number_of_trackpoints_per_user_query"
+        self.cursor.execute(query % number_of_trackpoints_per_user)
+        result = self.cursor.fetchall()
+        print(f"Average, maximum and minimum trackpoints per user")
+
+    def query_three(self):
+        query = "SELECT User.id, " \
+                "   COUNT(Activity.id) AS Number_of_activities  " \
+                "FROM User " \
+                "INNER JOIN Activity ON User.id = Activity.user_id " \
+                "GROUP BY User.id " \
+                "ORDER BY Number_of_activities DESC " \
+                "LIMIT 15"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(f"Top 15 users with the highest number of activities: \n{tabulate(result, ['User ID', 'Number of Activities'])}")
+
+    def query_four(self):
+        query = "SELECT DISTINCT User.id " \
+                "FROM User " \
+                "INNER JOIN Activity ON User.id = Activity.user_id " \
+                "WHERE Activity.transportation_mode = 'bus'"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(f"Users taken the bus: \n{result}")
 
 
 def main():
     program = None
     try:
         program = Queries()
-        _ = program.fetch_data(table_name="Activity")
+        table_names = ["Activity", "TrackPoint", "User"]
+
+        for name in table_names:
+            program.query_one(table_name=name)
+
+        program.query_two()
+        program.query_three()
+        program.query_four()
+
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
