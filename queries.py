3ed2ed005ced9ade0e4d2c80f79ce0206c288250
Eirik Steira <steiraeirik@gmail.com>
2023-10-01 17:59:22 +0200

    Implement queries 10, 11, 12

--- queries.py
+++ queries.py
@@ -74,6 +74,99 @@ class Queries:
         result = self.cursor.fetchall()
         print(f"duplicates: \n{tabulate(result)} ")
 
+    def query_10_longest_distances_per_transportation_mode_per_day(self):
+        query = """
+            SELECT
+                user_id,
+                transportation_mode,
+                travel_date,
+                distance AS max_distance
+            FROM
+                (
+                SELECT
+                    a.user_id,
+                    a.transportation_mode,
+                    DATE(a.start_date_time) AS travel_date,
+                    MAX(ST_Distance_Sphere(point(t1.lon, t1.lat), point(t2.lon, t2.lat))) AS distance
+                FROM
+                    Activity a
+                JOIN
+                    TrackPoint t1 ON a.id = t1.activity_id
+                JOIN
+                    TrackPoint t2 ON a.id = t2.activity_id
+                WHERE
+                    DATE(a.start_date_time) = DATE(t1.date_time) AND
+                    DATE(a.start_date_time) = DATE(t2.date_time) AND
+                    t1.date_time < t2.date_time
+                GROUP BY
+                    a.user_id,
+                    a.transportation_mode,
+                    travel_date
+            );
+        """
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(f"Users that have traveled the longest total distance in one day for each transportation mode: \n{tabulate(result)} ")
+
+
+    def query_11_users_with_invalid_activities(self):
+        query = """
+        SELECT
+            a.user_id,
+            COUNT(a.id)
+        FROM
+            Activity a
+        JOIN TrackPoint t1 ON
+            a.id = t1.activity_id
+        JOIN TrackPoint t2 ON
+            a.id = t2.activity_id
+        WHERE
+            t2.date_time > t1.date_time AND TIMESTAMPDIFF(
+                MINUTE,
+                t1.date_time,
+                t2.date_time
+            ) > 5
+        GROUP BY
+            a.user_id
+        """
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(f"All users with invalid activities: \n{tabulate(result)} ")
+
+    def query_12_users_with_their_most_used_transportation_mode(self):
+        query = """
+            SELECT
+                user_id,
+                transportation_mode
+            FROM
+                (
+                SELECT
+                    user_id,
+                    transportation_mode,
+                    ROW_NUMBER() OVER(
+                    PARTITION BY user_id
+                ORDER BY
+                    COUNT(*)
+                DESC
+                ) AS activity_rank
+            FROM
+                Activity
+            WHERE
+                transportation_mode IS NOT NULL
+            GROUP BY
+                user_id,
+                transportation_mode) AS ranked_activities
+                WHERE
+                    activity_rank = 1
+                GROUP BY
+                    user_id,
+                    transportation_mode
+                ORDER BY
+                    user_id;
+        """
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(f"All users with registered transportation modes and their most used transportation mode: \n{tabulate(result)} ")
 
 
 def main():
@@ -90,6 +183,10 @@ def main():
         program.query_four()
         program.query_five()
         program.query_six()
+
+        program.query_10_longest_distances_per_transportation_mode_per_day()
+        program.query_11_users_with_invalid_activities()
+        program.query_12_users_with_their_most_used_transportation_mode()
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
