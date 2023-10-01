db7d1cc1ca7d98b2470aea6f41bc24385979f832
Jørgen Halse <jrgenhalse@gmail.com>
2023-10-01 17:16:34 +0200

    do query 7 and start number 8

--- queries.py
+++ queries.py
@@ -1,5 +1,10 @@
+import argparse
 from tabulate import tabulate
 from DbConnector import DbConnector
+import pandas as pd
+import numpy as np
+from scipy.spatial import cKDTree
+from geopy.distance import geodesic
 
 
 class Queries:
@@ -74,22 +79,97 @@ class Queries:
         result = self.cursor.fetchall()
         print(f"duplicates: \n{tabulate(result)} ")
 
+    def query_seven(self):
+        subquery = "select user_id, transportation_mode, start_date_time, end_date_time " \
+                "from Activity " \
+                "where DATE(end_date_time) > DATE(start_date_time)"
+                                
+        query_a = "select COUNT(DISTINCT user_id) AS user_count " \
+                "from ( %s ) as subquery "
+        self.cursor.execute(query_a % subquery)
+        result_a = self.cursor.fetchall()
+
+        print(f"Number of users who have activities spanning two dates: \n{tabulate(result_a)}")
+
+        query_b = "SELECT user_id, transportation_mode, TIMESTAMPDIFF(SECOND, start_date_time, end_date_time) AS duration " \
+                "FROM ( %s ) AS subquery "
+        
+        self.cursor.execute(query_b % subquery)
+        result_b = self.cursor.fetchall()
+        print(f"Duration of activities spanning two dates: \n{tabulate(result_b)}")
+
+    # Assuming that the task wants us to find each single person who has been close to any other person both in time and space
+    def query_eight(self):
+        query = (
+            "SELECT * "
+            "FROM TrackPoint "
+            "INNER JOIN Activity ON TrackPoint.activity_id = Activity.id "
+            "GROUP BY Activity.user_id, TrackPoint.latitude, TrackPoint.longitude "
+        )
+        df = pd.read_sql(query, self.db_connection)
+        
+        # Convert lat/long to meters
+        df['coordinates_meters'] = df.apply(
+            lambda row: geodesic((0, 0), (row['latitude'], row['longitude'])).meters, axis=1
+        )
+        
+        # Split into separate x and y columns for easier use with cKDTree
+        df[['x', 'y']] = pd.DataFrame(df['coordinates_meters'].tolist(), index=df.index)
+        
+        # Build a k-d tree for efficient nearest neighbor search
+        tree = cKDTree(df[['x', 'y']].values)
+        
+        # Query the tree to find points within 50 meters of each other
+        pairs = tree.query_pairs(50)
+        
+        # Extract indices of rows to keep
+        indices_to_keep = set()
+        for pair in pairs:
+            indices_to_keep.add(pair[0])
+            indices_to_keep.add(pair[1])
+        
+        # Filter dataframe to keep only rows within 50 meters of another row
+        df_filtered = df.iloc[list(indices_to_keep)]
+        
+        return df_filtered
+        print(df_filtered)
 
 
-def main():
+
+def main(query: int):
     program = None
     try:
         program = Queries()
         table_names = ["Activity", "TrackPoint", "User"]
+        # cleanly run queries based on argument
+        if query == 1:
+            for name in table_names:
+                program.query_one(table_name=name)
+        elif query == 2:
+            program.query_two()
+        elif query == 3:
+            program.query_three()
+        elif query == 4:
+            program.query_four()
+        elif query == 5:
+            program.query_five()
+        elif query == 6:
+            program.query_six()
+        elif query == 7:
+            program.query_seven()
+        elif query == 8:
+            program.query_eight()
+        else:
+            print("ERROR: Invalid query number")
 
-        for name in table_names:
-            program.query_one(table_name=name)
+        # for name in table_names:
+        #     program.query_one(table_name=name)
 
-        program.query_two()
-        program.query_three()
-        program.query_four()
-        program.query_five()
-        program.query_six()
+        # program.query_two()
+        # program.query_three()
+        # program.query_four()
+        # program.query_five()
+        # program.query_six()
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
@@ -99,4 +179,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # Use args to be able to choose which query you want to run
+    parser = argparse.ArgumentParser(description="Choose query")
+    parser.add_argument("-query", type=int, help="Choose query")
+    args = parser.parse_args()
+    main(args.query)
