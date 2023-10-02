06fbcc0f01f631b4edc5fc7be7ac81e6354bf24e
Eirik Steira <steiraeirik@gmail.com>
2023-10-02 13:42:52 +0200

    fix queries

--- queries.py
+++ queries.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from tabulate import tabulate
 from DbConnector import DbConnector
 
@@ -76,62 +77,66 @@ class Queries:
 
     def query_10_longest_distances_per_transportation_mode_per_day(self):
         query = """
-            SELECT
-                user_id,
+             SELECT 
                 transportation_mode,
-                travel_date,
-                distance AS max_distance
-            FROM
-                (
+                user_id,
+                MAX(total_distance) AS max_distance
+            FROM (
                 SELECT
                     a.user_id,
                     a.transportation_mode,
-                    DATE(a.start_date_time) AS travel_date,
-                    MAX(ST_Distance_Sphere(point(t1.lon, t1.lat), point(t2.lon, t2.lat))) AS distance
+                    DATE(tp1.date_time) as travel_date,
+                    SUM(
+                        ST_DISTANCE_SPHERE(
+                            POINT(tp1.lon, tp1.lat),
+                            POINT(tp2.lon, tp2.lat)
+                        ) / 1000 
+                    ) AS total_distance
                 FROM
                     Activity a
+                JOIN 
+                    TrackPoint tp1 ON a.id = tp1.activity_id
                 JOIN
-                    TrackPoint t1 ON a.id = t1.activity_id
-                JOIN
-                    TrackPoint t2 ON a.id = t2.activity_id
+                    TrackPoint tp2 ON a.id = tp2.activity_id AND tp1.id = tp2.id - 1
                 WHERE
-                    DATE(a.start_date_time) = DATE(t1.date_time) AND
-                    DATE(a.start_date_time) = DATE(t2.date_time) AND
-                    t1.date_time < t2.date_time
-                GROUP BY
+                    a.transportation_mode IS NOT NULL
+                GROUP BY 
                     a.user_id,
                     a.transportation_mode,
-                    travel_date
-            );
+                    DATE(tp1.date_time)
+            ) AS distances
+            GROUP BY
+                transportation_mode, user_id;
         """
-        self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        print(f"Users that have traveled the longest total distance in one day for each transportation mode: \n{tabulate(result)} ")
+        result = pd.read_sql_query(query, self.db_connection)
+        result = result.sort_values('max_distance', ascending=False).drop_duplicates(['transportation_mode'])
+
+        print(f"Users that have traveled the longest total distance in one day for each transportation mode: \n{tabulate(result, headers=['transportation_mode', 'user_id', 'distance (km)'])} ")
 
 
     def query_11_users_with_invalid_activities(self):
         query = """
-        SELECT
-            a.user_id,
-            COUNT(a.id)
-        FROM
-            Activity a
-        JOIN TrackPoint t1 ON
-            a.id = t1.activity_id
-        JOIN TrackPoint t2 ON
-            a.id = t2.activity_id
-        WHERE
-            t2.date_time > t1.date_time AND TIMESTAMPDIFF(
-                MINUTE,
-                t1.date_time,
-                t2.date_time
-            ) > 5
-        GROUP BY
-            a.user_id
+            SELECT
+                a.user_id,
+                COUNT(a.id) as invalid_activities
+            FROM
+                Activity a
+            JOIN TrackPoint t1 ON
+                a.id = t1.activity_id
+            JOIN TrackPoint t2 ON
+                a.id = t2.activity_id
+                AND t1.id = t2.id - 1
+                AND TIMESTAMPDIFF(
+                    MINUTE,
+                    t1.date_time,
+                    t2.date_time
+                ) > 5
+            GROUP BY
+                a.user_id;
         """
         self.cursor.execute(query)
         result = self.cursor.fetchall()
-        print(f"All users with invalid activities: \n{tabulate(result)} ")
+        print(f"All users with invalid activities: \n{tabulate(result, headers=['user_id', 'invalid_activities'])} ")
 
     def query_12_users_with_their_most_used_transportation_mode(self):
         query = """
@@ -166,7 +171,7 @@ class Queries:
         """
         self.cursor.execute(query)
         result = self.cursor.fetchall()
-        print(f"All users with registered transportation modes and their most used transportation mode: \n{tabulate(result)} ")
+        print(f"All users with registered transportation modes and their most used transportation mode: \n{tabulate(result, headers=['user_id', 'transportation_mode'])} ")
 
 
 def main():
@@ -175,14 +180,14 @@ def main():
         program = Queries()
         table_names = ["Activity", "TrackPoint", "User"]
 
-        for name in table_names:
-            program.query_one(table_name=name)
-
-        program.query_two()
-        program.query_three()
-        program.query_four()
-        program.query_five()
-        program.query_six()
+        # for name in table_names:
+        #     program.query_one(table_name=name)
+        #
+        # program.query_two()
+        # program.query_three()
+        # program.query_four()
+        # program.query_five()
+        # program.query_six()
 
         program.query_10_longest_distances_per_transportation_mode_per_day()
         program.query_11_users_with_invalid_activities()
